cf6444b73033a9827daa4cb2edc273243a53978c
PolukondaDharani <polukondaddharani@gmail.com>
2022-09-08 17:56:22 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class First{
+    public static void main(String[] args){
+      //  System.out.println("hello world");
+     // System.out.println(args[0]);
+    Scanner in = new Scanner(System.in);
+     int t = in.nextInt();
+     System.out.println(t); 
+     //for(int i=0;i<4;i++){
+     //   int v=in.nextInt();
+      //  System.out.println(v);
+     }
+     float r = 45.67f;
+     int y = (int)(r);
+     System.out.println(y);
+    }
+}
